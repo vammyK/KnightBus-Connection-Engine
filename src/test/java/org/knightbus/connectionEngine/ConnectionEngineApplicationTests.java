0e8fc1ee709e241d53bb1c74898b710d9ab6ef7f
Vammy <vamshi.akundi@outlook.com>
2019-12-13 06:09:56 +0800

    Init Commit

---
+++ src/test/java/org/knightbus/connectionEngine/ConnectionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.knightbus.connectionEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
